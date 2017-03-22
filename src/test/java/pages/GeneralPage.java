c8664f21a3a3493a0e200b5ff4e08003020ed564
MrBlackk <volodymyr.chornyj@gmail.com>
2017-03-23 00:49:47 +0200

    - scroll to elements

--- src/test/java/pages/GeneralPage.java
+++ src/test/java/pages/GeneralPage.java
@@ -6,6 +6,10 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.PageFactory;
 import utils.Log4Test;
 
@@ -79,5 +83,38 @@ abstract class GeneralPage {
      */
     protected void tap(MobileElement element){
         new TouchAction(appiumDriver).tap(element).perform();
+    }
+
+    public static void scrollToElement(AndroidDriver driver, String elementName, boolean scrollDown) {
+        String listID = ((RemoteWebElement) driver.findElementByAndroidUIAutomator("new UiSelector().className(\"android.widget.ListView\")")).getId();
+        String direction;
+        if (scrollDown) {
+            direction = "down";
+        } else {
+            direction = "up";
+        }
+        HashMap<String, String> scrollObject = new HashMap<String, String>();
+        scrollObject.put("direction", direction);
+        scrollObject.put("element", listID);
+        scrollObject.put("text", elementName);
+        driver.executeScript("mobile: scrollTo", scrollObject);
+    }
+
+    public MobileElement scrollToElementByName(String elementName, String listId) {
+        MobileElement element = (MobileElement) ((AndroidDriver) appiumDriver).findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()" +
+                ".resourceId(\"" + listId + "\"))" +
+                ".scrollIntoView(new UiSelector().text(\"" + elementName + "\"));");
+        return element;
+    }
+
+    public void scrollToElement(String elementName) {
+        while (appiumDriver.findElements(By.name(elementName)).size() == 0) {
+            Dimension dimensions = appiumDriver.manage().window().getSize();
+            Double screenHeightStart = dimensions.getHeight() * 0.5;
+            int scrollStart = screenHeightStart.intValue();
+            Double screenHeightEnd = dimensions.getHeight() * 0.2;
+            int scrollEnd = screenHeightEnd.intValue();
+            appiumDriver.swipe(0, scrollStart, 0, scrollEnd, 5000);
+        }
     }
 }
